3797188f4dad8db4a657a3ba1be6869b421d3cd7
Amanda Aurora <amandeaurora@gmail.com>
2023-09-04 10:32:02 +0200

    Aligning with API

--- pgn_chess_games/model/main.py
+++ pgn_chess_games/model/main.py
@@ -34,7 +34,7 @@ LOCAL_DATA_PATH = os.path.join(os.environ["LOCAL_DATA_PATH"])
 epochs = 50
 
 
-def main():
+def main_IAM():
     ## Define the data with shuffle function
     np.random.seed(42)
     tensorflow.random.set_seed(42)
@@ -142,3 +142,102 @@ def get_predictions_decoded(batch):
     pred_texts = decode_batch_predictions(predictions)
 
     return pred_texts
+
+
+def train_chess():
+    np.random.seed(42)
+    tensorflow.random.set_seed(42)
+
+    base_path = "/root/code/laligb/pgn-chess-games/data/extracted_move_boxes"
+
+    train_words = open(f"{base_path}/train_data.txt", "r").readlines()
+    train_samples = []
+    for line in train_words:
+        if line[0] == "#":
+            continue
+        if line.split(" ")[1] != "err":  # We don't need to deal with errored entries.
+            train_samples.append(line.replace("\n", ""))
+
+    val_words = open(f"{base_path}/val_data.txt", "r").readlines()
+    validation_samples = []
+    for line in val_words:
+        if line[0] == "#":
+            continue
+        if line.split(" ")[1] != "err":  # We don't need to deal with errored entries.
+            validation_samples.append(line.replace("\n", ""))
+
+    test_words = open(f"{base_path}/testing_tags.txt", "r").readlines()
+    test_samples = []
+    for line in test_words:
+        if line[0] == "#":
+            continue
+        if line.split(" ")[1] != "err":  # We don't need to deal with errored entries.
+            test_samples.append(line.replace("\n", ""))
+    test_samples = validation_samples
+
+    print(f"Total training samples: {len(train_samples)}")
+    print(f"Total validation samples: {len(validation_samples)}")
+    print(f"Total test samples: {len(test_samples)}")
+
+    base_image_path = os.path.join(base_path)
+
+    train_img_paths, train_labels = get_image_paths_and_labels(train_samples)
+    validation_img_paths, validation_labels = get_image_paths_and_labels(
+        validation_samples
+    )
+    test_img_paths, test_labels = get_image_paths_and_labels(test_samples)
+
+    train_labels_cleaned = []
+    characters = set()
+    max_len = 0
+
+    for label in train_labels:
+        label = label.split(" ")[-1].strip()
+        for char in label:
+            characters.add(char)
+
+        max_len = max(max_len, len(label))
+        train_labels_cleaned.append(label)
+
+    characters = sorted(list(characters))
+
+    print("Maximum length: ", max_len)
+    print("Vocab size: ", len(characters))
+
+    validation_labels_cleaned = cleaning_labels(validation_labels)
+    test_labels_cleaned = cleaning_labels(test_labels)
+
+    AUTOTUNE = tensorflow.data.AUTOTUNE
+    with open("characters.txt", "w") as f:
+        for char in characters:
+            f.write("%s\n" % char)
+
+    # Mapping characters to integers.
+    char_to_num = StringLookup(vocabulary=list(characters), mask_token=None)
+
+    # Mapping integers back to original characters.
+    num_to_char = StringLookup(
+        vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True
+    )
+
+    train_ds = prepare_dataset(train_img_paths, train_labels_cleaned)
+    validation_ds = prepare_dataset(validation_img_paths, validation_labels_cleaned)
+    test_ds = prepare_dataset(test_img_paths, test_labels_cleaned)
+
+    print(f"⏳ Initializing model")
+    img_size = (128, 32)
+    model, prediction_model = initialize_model(img_size)
+    edit_distance_callback = EditDistanceCallback(prediction_model, validation_ds)
+    print(f"✅ Model initialized")
+
+    # Train the model.
+    print(f"⏳ Training model with {epochs} epochs")
+
+    history = model.fit(
+        train_ds,
+        validation_data=validation_ds,
+        epochs=epochs,
+        callbacks=[edit_distance_callback],
+    )
+
+    save_model(prediction_model)
